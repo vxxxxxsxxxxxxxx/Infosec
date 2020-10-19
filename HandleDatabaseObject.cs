719c0516a9f510f9450e3da0e2410a680ba5943d
vxxxxx <vxxxxxsxxxxxxxx@protonmail.com>
2020-10-19 18:48:05 +0300

    Create HandleDatabaseObject.cs

---
+++ HandleDatabaseObject.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+/// 
+public class DatabaseObject()
+{
+	private Dictionary<string, string> phenomenon;
+	
+	public Dictionary<string, string> getPhenomenon()
+	{
+		return this.phenomenon;
+	}
+	
+	public setPhenomenon(Dictionary<string, string> inputPhenomenon)
+	{
+		this.phenomenon = inputPhenomenon;
+	}
+	
+	public string getFieldContentFromPhenomenon(string inputFieldName)
+	{
+		return this.phenomenon.TryGetValue(inputFieldName);
+	}
+	
+	public void setFieldContentFromPhenomenon(string inputFieldName, string inputFieldContent)
+	{
+		this.phenomenon[inputFieldName] = inputFieldContent;
+	}
+}
+
+public class DatabaseObjects()
+{
+	public DatabaseObject[] databaseObjects;
+}
