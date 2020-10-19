01ce7bfb3931f96599f09a4bab0c16b5b0d7b6ed
Марина Титова <mapuho4ka_98@mail.ru>
2020-10-19 21:46:01 +0300

    initial project

--- HandleUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-public class Class1
-{
-	public string fio;
-	public string dr;
-	public string login;
-	public string parol;
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Infosec
-Modern Proggramming Technologies
